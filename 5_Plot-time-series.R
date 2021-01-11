8674cdf4bf8b40995dc64b2df3e5fea5d85cbf74
andrejeva-d <andrejeva.d.lv@gmail.com>
2021-01-11 23:03:03 +0100

    Add to time series plot

--- 5_Plot-time-series.R
+++ 5_Plot-time-series.R
@@ -1,4 +1,5 @@
 
+
 library(plotly)
 
 #########################################################################################
@@ -8,21 +9,15 @@ githubURL <-
   "https://github.com/andrejeva-d/dk-dash-covid/blob/main/Data/data_time_series.RData?raw=true"
 load(url(githubURL))
 
-
-
-
 #########################################################################################
 # PREPARE Death data
 
-
-colorscale2 = c(
-  list(c(0, "#292927")),#melns
-  list(c(0.2, "#264563")), #peleks
-  list(c(0.4, "#3a9fe8")),#zils
-  list(c(0.6, "#9391c4")),#dzeltens
-  list(c(0.8, "#c468ae")),#dzeltens
-  list(c(1, "#ba4a75")))#sarkans
-colorscale2
+colorscale2 = c(list(c(0, "#292927")), #melns
+                list(c(0.2, "#264563")), #peleks
+                list(c(0.4, "#3a9fe8")), #zils
+                list(c(0.6, "#9391c4")), #dzeltens
+                list(c(0.8, "#c468ae")), #dzeltens
+                list(c(1, "#ba4a75")))#sarkans
 
 font_list <- list(size = 12,
                   family = "13px-apple-system, Courier New, monospace",
@@ -44,23 +39,32 @@ xform <- list(
   showticklabels = T
 )
 
+plot_margin = list(
+  l = 35,
+  r = 5,
+  b = 0,
+  t = 20,
+  pad = 1
+)
+  
 head(tab)
-fit_Death <- fitted(loess(tab$Death ~ seq(1:dim(tab)[1]), span=0.1))
-fit_Admitted <- fitted(loess(tab$Adm ~ seq(1:dim(tab)[1]), span=0.1))
-fit_Positive <- fitted(loess(tab$Positive ~ seq(1:dim(tab)[1]), span=0.1))
-
-
+fit_Death <- fitted(loess(tab$Death ~ seq(1:dim(tab)[1]), span = 0.1))
+fit_Admitted <-
+  fitted(loess(tab$Adm ~ seq(1:dim(tab)[1]), span = 0.1))
+fit_Positive <-
+  fitted(loess(tab$Positive ~ seq(1:dim(tab)[1]), span = 0.1))
 
 #########################################################################################
 # Plot Positive
 
 yform <- list(
-  dtick = c(400),
+  dtick = c(500),
   fixedrange = FALSE,
   tickfont = tickfont_list,
   zeroline = T,
   showticklabels = T
 )
+
 
 fig <- plot_ly()
 fig <- fig %>% add_trace(
@@ -83,14 +87,19 @@ fig <- fig %>% add_trace(
   marker = list(colorscale = colorscale2)
 )
 
+
 fig_plot_1 <- fig %>% layout(
+  plot_layout,
   xaxis = xform,
   yaxis = yform,
   showlegend = FALSE,
-  margin = list(l = 35,r = 5,b = 0,t = 20,pad = 1),
+  paper_bgcolor = "#00000000",
+  plot_bgcolor = "#00000000",
+  font = font_list,
+  margin = plot_margin,
   annotations =
     list(
-      x = 0.96,
+      x = 0.06,
       y = 0.95,
       text = "Number of New Positive",
       showarrow = F,
@@ -99,59 +108,7 @@ fig_plot_1 <- fig %>% layout(
       font = font_list
     )
 )
-fig_plot_1 %>% hide_colorbar() %>% config(scrollZoom = TRUE)
-
-#########################################################################################
-# Plot Death
-
-yform <- list(
-  dtick = c(4),
-  fixedrange = FALSE,
-  tickfont = tickfont_list,
-  zeroline = T,
-  showticklabels = T
-)
-
-fig <- plot_ly()
-fig <- fig %>% add_trace(
-  type = "bar",
-  x = tab$Data,
-  y = tab$Death,
-  color = fit_Death,
-  hoverinfo = "text",
-  hovertext = paste0("Nr. of Deceased = ", tab$Death, "\n", tab$Data),
-  marker = list(opacity = 0.5, colorscale = colorscale2)
-)
-
-fig <- fig %>% add_trace(
-  mode = 'lines+markers',
-  x = tab$Data,
-  y = fit_Death,
-  color = fit_Death,
-  hoverinfo = "none",
-  line = list(color = "#000000"),
-  marker = list(colorscale = colorscale2)
-)
-
-fig_plot_3 <- fig %>% layout(
-  xaxis = xform,
-  yaxis = yform,
-  showlegend = FALSE,
-  margin = list(l = 35,r = 5,b = 0,t = 20,pad = 1),
-  annotations =
-    list(
-      x = 0.96,
-      y = 0.95,
-      text = "Number of Newly Admitted",
-      showarrow = F,
-      xref = 'paper',
-      yref = 'paper',
-      font = font_list
-    )
-)
-fig_plot_3 %>% hide_colorbar() %>% config(scrollZoom = TRUE)
-
-
+fig_plot_1 %>% config(scrollZoom = TRUE) %>% hide_colorbar()
 
 #########################################################################################
 # Plot Positive
@@ -186,13 +143,17 @@ fig <- fig %>% add_trace(
 )
 
 fig_plot_2 <- fig %>% layout(
+  plot_layout,
   xaxis = xform,
   yaxis = yform,
   showlegend = FALSE,
-  margin = list(l = 35,r = 5,b = 0,t = 20,pad = 1),
+  paper_bgcolor = "#00000000",
+  plot_bgcolor = "#00000000",
+  font = font_list,
+  margin = plot_margin,
   annotations =
     list(
-      x = 0.96,
+      x = 0.06,
       y = 0.95,
       text = "Number of Newly Admitted",
       showarrow = F,
@@ -201,14 +162,74 @@ fig_plot_2 <- fig %>% layout(
       font = font_list
     )
 )
-fig_plot_2 %>% hide_colorbar() %>% config(scrollZoom = TRUE)
 
+#########################################################################################
+# Plot Death
 
+yform <- list(
+  dtick = c(4),
+  fixedrange = FALSE,
+  tickfont = tickfont_list,
+  zeroline = T,
+  showticklabels = T
+)
 
+fig <- plot_ly()
+fig <- fig %>% add_trace(
+  type = "bar",
+  x = tab$Data,
+  y = tab$Death,
+  color = fit_Death,
+  hoverinfo = "text",
+  hovertext = paste0("Nr. of Deceased = ", tab$Death, "\n", tab$Data),
+  marker = list(opacity = 0.5, colorscale = colorscale2)
+)
 
+fig <- fig %>% add_trace(
+  mode = 'lines+markers',
+  x = tab$Data,
+  y = fit_Death,
+  color = fit_Death,
+  hoverinfo = "none",
+  line = list(color = "#000000"),
+  marker = list(colorscale = colorscale2)
+)
 
+fig_plot_3 <- fig %>% layout(
+  plot_layout,
+  xaxis = xform,
+  yaxis = yform,
+  showlegend = FALSE,
+  paper_bgcolor = "#00000000",
+  plot_bgcolor = "#00000000",
+  font = font_list,
+  margin = plot_margin,
+  annotations =
+    list(
+      x = 0.06,
+      y = 0.95,
+      text = "Number of Deceased",
+      showarrow = F,
+      xref = 'paper',
+      yref = 'paper',
+      font = font_list
+    )
+)
 
+#########################################################################################
+# Plot Positive
 
+library(htmltools)
+library(crosstalk)
 
-
+fig_comb <- subplot(
+  fig_plot_1,
+  fig_plot_2,
+  fig_plot_3,
+  margin = -0.04,
+  shareX = TRUE,
+  nrows = 3,
+  which_layout = c(1, 2, 3)
+)
+fig_comb %>% config(scrollZoom = TRUE) %>% hide_colorbar()
 
